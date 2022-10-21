b3851149fc9351a2d01233fa2c91d287def9e5e0
AHOUZI HASNAE <92023794+hasnae21@users.noreply.github.com>
2022-10-21 11:06:17 +0100

    save

--- prototypes/prototypeBasic/database/migrations/2022_10_21_092520_create_promotions_table.php
+++ prototypes/prototypeBasic/database/migrations/2022_10_21_092520_create_promotions_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('promotions', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            // $table->timestamps();
+            $table->timestamps();
         });
     }
 
