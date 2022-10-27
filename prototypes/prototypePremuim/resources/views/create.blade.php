66fdba9ba43369b19522c11a2abf180f672d0e5f
AHOUZI HASNAE <92023794+hasnae21@users.noreply.github.com>
2022-10-27 16:12:10 +0100

    save

--- prototypes/prototypePremuim/resources/views/create.blade.php
+++ prototypes/prototypePremuim/resources/views/create.blade.php
@@ -2,13 +2,13 @@
 @section('title')
 New Promotion 
 @endsection
-
 @section('content')
 
     <form action="/add" method="POST">
     @csrf
         <input type="text" id="new" name="new">
         <button >Ajouter</button>
+    </form>
 
     <div style="color:red;">
         @error('new')
